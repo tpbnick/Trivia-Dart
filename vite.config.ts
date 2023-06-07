351c0e6f80e8f954968e8adcadd7d5481b415e44
tpbnick <nickplatt4@gmail.com>
2023-06-06 23:25:13 -0400

    Removed unnecessary code from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __COMMIT_HASH__: JSON.stringify(import.meta.env && import.meta.env.VITE_GIT_SHA)
-  }
 })
