003db3515e0a53cc89557aaa342ea41357682459
Nick <49327729+tpbnick@users.noreply.github.com>
2023-08-03 20:00:11 -0400

    Move supabase URL to env

--- src/components/SupaBase.ts
+++ src/components/SupaBase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://jzpmvgewvzbumesuozcl.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_APIKEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
