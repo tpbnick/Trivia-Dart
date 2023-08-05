c96780cf164a44de2cb543dbe17b1e79fe7d12d4
Nick <49327729+tpbnick@users.noreply.github.com>
2023-08-04 23:01:10 -0400

    Update PWA Manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,27 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate', devOptions: {
+  plugins: [react(), VitePWA({ 
+    registerType: 'autoUpdate', devOptions: {
     enabled: true
-  } })],
+    },
+    includeAssets: [],
+    manifest: {
+      name: 'TriviaDart',
+      short_name: 'Trivia🎯',
+      description: 'Simple Trivia Application',
+      icons: [
+        {
+          src: '/src/assets/dart.webp',
+          sizes: '192x192',
+          type: 'image/webp'
+        },
+        {
+          src: '/src/assets/dart.webp',
+          sizes: '512x512',
+          type: 'image/webp'
+        }
+      ]
+    } 
+  })],
 })
