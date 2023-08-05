f300284bf66643cbdfef4b621f06382cb5cf9354
Nick <49327729+tpbnick@users.noreply.github.com>
2023-08-04 22:51:44 -0400

    Removed Ionic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.td0fo97vdfo"
+    "revision": "0.ksq82o2ebug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
