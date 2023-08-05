c47ee49ec7ab018f7d054e8ce42ff98cfcd77f74
Nick <49327729+tpbnick@users.noreply.github.com>
2023-08-05 11:49:22 -0400

    Update vite.config.ts and move image

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
       display: 'standalone',
       icons: [
         {
-          src: 'src/assets/dart.webp',
+          src: 'public/dart.webp',
           sizes: '192x192',
           type: 'image/webp'
         },
         {
-          src: 'src/assets/dart.webp',
+          src: 'public/dart.webp',
           sizes: '512x512',
           type: 'image/webp'
         }
