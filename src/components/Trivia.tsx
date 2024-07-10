282cc998b5f8283986eda553fe9cb009be38b577
Nick <49327729+tpbnick@users.noreply.github.com>
2024-07-10 15:14:28 -0700

    Added alert for rate limit for Open Trivia DB

--- src/components/Trivia.tsx
+++ src/components/Trivia.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
 import { useState, ChangeEvent } from "react";
 import About from "./About";
 import Settings from "./Settings";
@@ -269,6 +268,25 @@ const Trivia = () => {
 					</select>
 				</div>
 			</div>
+
+			{selectedSource === "Open Trivia DB" && (
+				<div role="alert" className="alert max-w-lg">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6 shrink-0 stroke-current"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+						/>
+					</svg>
+					<span>Open TriviaDB is limited to 1 question every 5 seconds.</span>
+				</div>
+			)}
 
 			<button
 				onClick={handleButtonClick}
